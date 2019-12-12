9400416f3d7226f53a7591f185f2e7bbb2f54f1a
yogymax <yogymax@gmail.com>
2019-12-12 11:34:19 +0530

    cmd commit

--- src/main/java/com/scp/java/App.java
+++ src/main/java/com/scp/java/App.java
@@ -6,9 +6,11 @@ package com.scp.java;
  */
 public class App 
 {
+	
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+    	System.out.println("hi");
+    	System.out.println( "Hello World!" );
         System.out.println("This is newly added...");
     }
 }
