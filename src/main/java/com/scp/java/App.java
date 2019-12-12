4078bc956b54a989c3948d3e519efc902957e6ac
yogymax <yogymax@gmail.com>
2019-12-12 11:35:59 +0530

    Update App.java

--- src/main/java/com/scp/java/App.java
+++ src/main/java/com/scp/java/App.java
@@ -9,7 +9,7 @@ public class App
 	
     public static void main( String[] args )
     {
-    	System.out.println("hi");
+    	System.out.println("hi...1");
     	System.out.println( "Hello World!" );
         System.out.println("This is newly added...");
     }
